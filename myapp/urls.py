75062a04c0a811d17a66fd9f83e09b0d4d96e935
bibongbenh04 <hongquan29102004@gmail.com>
2024-01-10 03:58:05 +0700

    add quiz

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
 	path('', views.index, name = 'index'),
-	path('register', views.register, name = 'register'),
 	path('login', views.login, name = 'login'),
 	path('signup', views.signup, name = 'signup'),
 	path('logout', views.logout, name = 'logout'),
--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+from django.contrib import messages as django_messages
 from .models import Feature, QuizQuestion, ThreadManager, Thread
 from myapp.models import Thread
 
@@ -49,31 +49,28 @@ def logout(request):
 	return redirect('/')
 
 def signup(request):
-	return render(request, 'themes/signup.html')
-
-def register(request):
 	if request.method == 'POST':
 		username = request.POST['username']
 		email = request.POST['email']
 		password = request.POST['password']
-		password2 = request.POST['password2']
+		password2 = request.POST['confirm_password']
 
 		if password == password2:
 			if User.objects.filter(email=email).exists():
-				messages.info(request, 'Email Already Used')
-				return redirect('register')
+				django_messages.info(request, 'Email Already Used')
+				return redirect('signup')
 			elif User.objects.filter(username=username).exists():
-				messages.info(request, 'Username Already Used')
-				return redirect('register')
+				django_messages.info(request, 'Username Already Used')
+				return redirect('signup')
 			else:
 				user = User.objects.create_user(username=username, email=email, password=password)
 				user.save()
 				return redirect('login')
 		else:
-			messages.info(request, 'Password Not The Same')
-			return redirect('register')
+			django_messages.info(request, 'Password Not The Same')
+			return redirect('signup')
 	else:
-		return render(request, 'register.html')
+		return render(request, 'themes/signup.html')
 
 def post(request, pk):
 	return render(request, 'post.html', {'pk': pk})
